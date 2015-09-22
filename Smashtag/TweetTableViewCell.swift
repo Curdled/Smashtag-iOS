fbd9e3b5e8b274d52a5334640331a287b9771180
Joe Isaacs <joe.isaacs@live.co.uk>
2015-09-22 16:35:09 +0100

    Required Task 1

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -15,37 +15,43 @@ class TweetTableViewCell: UITableViewCell
             updateUI()
         }
     }
-    
+
     @IBOutlet weak var tweetProfileImageView: UIImageView!
     @IBOutlet weak var tweetScreenNameLabel: UILabel!
     @IBOutlet weak var tweetTextLabel: UILabel!
     @IBOutlet weak var tweetCreatedLabel: UILabel!
-    
+
     func updateUI() {
         // reset any existing tweet information
         tweetTextLabel?.attributedText = nil
         tweetScreenNameLabel?.text = nil
         tweetProfileImageView?.image = nil
         tweetCreatedLabel?.text = nil
-        
+
         // load new information from our tweet (if any)
+
         if let tweet = self.tweet
         {
-            tweetTextLabel?.text = tweet.text
-            if tweetTextLabel?.text != nil  {
-                for _ in tweet.media {
-                    tweetTextLabel.text! += " 📷"
-                }
+            if let var text = tweet.text {
+            for _ in tweet.media {
+                text += " 📷"
             }
-            
+
+            let newTweet = NSMutableAttributedString(string: text)
+
+            newTweet.colorStringAtIndexs(UIColor.redColor(), indexs: tweet.hashtags)
+            newTweet.colorStringAtIndexs(UIColor.blueColor(), indexs: tweet.urls)
+            newTweet.colorStringAtIndexs(UIColor.orangeColor(), indexs: tweet.userMentions)
+
+            tweetTextLabel?.attributedText = newTweet
             tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
-            
+
             if let profileImageURL = tweet.user.profileImageURL {
                 if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
                     tweetProfileImageView?.image = UIImage(data: imageData)
                 }
             }
-            
+
             let formatter = NSDateFormatter()
             if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
                 formatter.dateStyle = NSDateFormatterStyle.ShortStyle
@@ -54,6 +60,17 @@ class TweetTableViewCell: UITableViewCell
             }
             tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
         }
-        
+        }
+
+    }
+
+}
+
+
+private extension NSMutableAttributedString {
+    func colorStringAtIndexs(color: UIColor, indexs: [Tweet.IndexedKeyword]) {
+        for index in indexs {
+            addAttribute(NSForegroundColorAttributeName, value: color, range: index.nsrange)
+        }
     }
 }
